10c3e16939106f346b9e9715c002d344a23cb84c
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2025-09-14 02:15:11 +0300

    Feat: Integration Tours Api to get tours details

--- src/components/profile/MyBooking/Tours.tsx
+++ src/components/profile/MyBooking/Tours.tsx
@@ -1,28 +1,34 @@
 import BorderedContainer from "../../common/BorderedContainer";
 import { Card, CardContent } from "../../ui/card";
-import tour from "../../../assets/tour.png";
 import { Star } from "lucide-react";
+import type { Tour } from "@/Types/Tours";
 
-export default function Tours() {
+export default function Tours({ tourDetails }: { tourDetails: Tour }) {
   return (
     <>
       <BorderedContainer className="my-6">
         <Card className="md:p-4 p-2">
           <CardContent className="flex gap-4 px-0">
-            <div>
-              <img src={tour} alt="Car Image" className="object-cover w-36" />
-            </div>
-            <div className="flex  justify-between w-full">
+            <img
+              src={tourDetails.image}
+              alt="Tour Image"
+              className="object-cover w-36 rounded-lg"
+            />
+            <div className="flex  justify-between w-full text-sm md:text-lg">
               <div className="flex flex-col gap-2 justify-center">
-                <p className="text-md md:text-xl font-semibold text-muted">
-                  Full Day Tour
+                <p className="text-md md:text-xl text-muted">
+                  {tourDetails.title}
                 </p>
-                <p className="text-md md:text-2xl">Luxor</p>
-                <p className="text-muted text-md font-semibold">
-                  From <span className="text-primary">$100</span> per person
+                <p className="text-md md:text-2xl">{tourDetails.location}</p>
+                <p className="text-muted text-md">
+                  From{" "}
+                  <span className="text-primary">
+                    ${Number(tourDetails.price).toFixed(0)}
+                  </span>{" "}
+                  per person
                 </p>
               </div>
-              <p className="flex gap-2 font-semibold text-sm md:text-md">
+              <p className="flex flex-col sm:flex-row gap-2 text-sm md:text-lg">
                 <Star className="w-5 h-5 text-secondary fill-secondary" />
                 4.3
               </p>
--- src/pages/profile/MyBooking/MyToursBookingPage.tsx
+++ src/pages/profile/MyBooking/MyToursBookingPage.tsx
@@ -1,9 +1,80 @@
+import type { Tour } from "@/Types/Tours";
 import Tours from "@/components/profile/MyBooking/Tours";
+import axios from "axios";
+import { Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export default function MyToursBookingPage() {
+  const [loading, setLoading] = useState(false);
+  const [bookedToursIDs, setBookedToursIDs] = useState<number[]>([]);
+  const [toursDetails, setToursDetails] = useState<Tour[]>([]);
+
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+  const token = localStorage.getItem("token");
+
+  const getAllBookedTours = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(`${BASE_URL}my-tour-bookings`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (response.status === 200) {
+        const ids = response.data.data.map((tour: Tour) => tour.tour_id);
+        setBookedToursIDs(ids);
+      }
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  const getToursDetails = async (ids: number[]) => {
+    try {
+      setLoading(true);
+      const requests = ids.map((id) =>
+        axios.get(`${BASE_URL}tours/${id}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+      );
+      const responses = await Promise.all(requests);
+      if (responses.every((res) => res.status === 200)) {
+        toast.success("Booked Tours fetched successfully");
+        const toursData = responses.map((res) => res.data.data);
+        setToursDetails(toursData);
+        console.log(toursData);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+      toast.error("Failed to fetch booked tours details");
+    }
+  };
+
+  useEffect(() => {
+    getAllBookedTours();
+  }, []);
+
+  useEffect(() => {
+    if (bookedToursIDs.length > 0) {
+      getToursDetails(bookedToursIDs);
+    }
+  }, [bookedToursIDs]);
+
   return (
     <>
-      <Tours />
+      {loading ? (
+        <div className="flex items-center justify-center h-[calc(100vh-200px)]">
+          <Loader2 className="animate-spin text-primary w-20 h-20" />
+        </div>
+      ) : bookedToursIDs.length === 0 ? (
+        <h1>No Tours Booked</h1>
+      ) : (
+        toursDetails.map((tour: Tour) => (
+          <Tours key={tour.id} tourDetails={tour} />
+        ))
+      )}
     </>
   );
 }
